46e94da7e8c21f6c4a42d5e42ddd76462159f4e6
Nicolas Boutin <nicolas.boutin@external.magnetimarelli.com>
2023-08-07 09:09:34 +0200

    Rename simple_ass_mat

---
+++ simple_ass_mat/__main__.py
@@ -0,0 +1,27 @@
+"""
+:author Nicolas Boutin
+:date 2023-07-04
+"""
+# pylint: disable=logging-fstring-interpolation
+
+import logging
+# import yaml
+
+# from controller import factory
+
+__NAME = "Free AssMat"
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """main"""
+    logging.basicConfig(level=logging.DEBUG)
+    logger.info(f"Running {__NAME}")
+
+    # with open("data.yml", "r", encoding="utf-8") as file:
+    #     data = yaml.safe_load(file)
+
+
+if __name__ == "__main__":
+    main()
