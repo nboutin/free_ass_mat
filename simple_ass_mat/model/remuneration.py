accdae1e8d27a62a245f7c6375dd5c2fafc29fd7
nboutin <boutwork@gmail.com>
2023-08-18 15:55:25 +0200

    Add Remuneration tarif_horaire_net

--- simple_ass_mat/model/remuneration.py
+++ simple_ass_mat/model/remuneration.py
@@ -8,6 +8,8 @@ Description:
 class Remuneration:
     """Handle ass mat remuneration"""
 
+    _RATIO_BRUT_NET = 0.7812
+
     def __init__(self, tarif_horaire_brut: float) -> None:
         self._tarif_horaire_brut = tarif_horaire_brut
 
@@ -15,3 +17,8 @@ class Remuneration:
     def tarif_horaire_brut(self):
         """Retourne le tarif horaire brut"""
         return self._tarif_horaire_brut
+
+    @property
+    def tarif_horaire_net(self):
+        """Retourne le tarif horaire net"""
+        return self._tarif_horaire_brut * Remuneration._RATIO_BRUT_NET
---
+++ test/model/test_remuneration.py
@@ -0,0 +1,39 @@
+"""
+:date 2023-08
+:author Nicolas Boutin
+"""
+# pylint: disable=logging-fstring-interpolation
+# pylint: disable=wrong-import-position
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+import unittest
+import sys
+import os
+from datetime import time
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+
+from simple_ass_mat.model.remuneration import Remuneration  # nopep8 # noqa: E402
+
+
+class TestTarifHoraireNet(unittest.TestCase):
+
+    def test_brut_4_10(self):
+        """
+        tarif horaire brut = 4.10€
+        """
+        remuneration = Remuneration(4.10)
+        self.assertAlmostEqual(remuneration.tarif_horaire_net, 3.2029, delta=0.0001)
+
+
+if __name__ == '__main__':
+    import logging
+    from pathlib import Path
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(Path(__file__).parent / Path(__file__ + '.log'), mode='w')])
+
+    unittest.main()
